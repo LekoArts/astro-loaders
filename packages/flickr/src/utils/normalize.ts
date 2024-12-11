e2dcf470fa4198291b3bc1dbe1c195ae5bd3b1f7
LekoArts <lekoarts@gmail.com>
2024-12-11 10:52:08 +0100

    improve normalize

--- packages/flickr/src/utils/normalize.ts
+++ packages/flickr/src/utils/normalize.ts
@@ -1,9 +1,9 @@
 import type { z } from 'astro/zod'
-import type { NormalizedPhoto } from '../schema.js'
-import type { FlickrPhoto } from '../types/flickr'
+import type { PeopleGetPhotos, PhotosetsGetList } from '../schema.js'
+import type { FlickrResponse } from '../types/flickr'
 import { SIZES } from '../constants.js'
 
-type Photo = z.infer<typeof NormalizedPhoto>
+type Res = z.infer<typeof PeopleGetPhotos> & z.infer<typeof PhotosetsGetList>
 type SizesArray = Array<keyof typeof SIZES>
 type SizesUnion = keyof typeof SIZES
 
@@ -17,14 +17,13 @@ interface ImageUrl {
 /**
  * The responses you get back from Flickr's API are a mess. They mix naming conventions, different data types for the same category, and so on. This function tries to normalize the data a bit.
  */
-export function normalizePhoto(photo: FlickrPhoto): Photo {
-  const { server, farm, secret, ...rest } = photo
-  const copy = { ...rest } as Partial<FlickrPhoto>
+export function normalize(res: FlickrResponse): Res {
+  const { server, farm, secret, ...rest } = res
+  const copy = { ...rest } as Partial<FlickrResponse>
   const output = {
     id: rest.id,
     owner: rest.owner,
-    title: rest.title,
-  } as Photo
+  } as Res
 
   ;(Object.keys(SIZES) as SizesArray).forEach((suffix) => {
     if (Object.prototype.hasOwnProperty.call(copy, `height_${suffix}`)) {
@@ -64,7 +63,7 @@ export function normalizePhoto(photo: FlickrPhoto): Photo {
   }
 
   if (Object.prototype.hasOwnProperty.call(copy, 'views')) {
-    if (copy.views) {
+    if (typeof copy.views !== 'undefined') {
       output.views = Number.parseInt(copy.views, 10)
     }
   }
@@ -75,6 +74,66 @@ export function normalizePhoto(photo: FlickrPhoto): Photo {
 
   if (Object.prototype.hasOwnProperty.call(copy, 'media_status')) {
     output.media_status = copy.media_status
+  }
+
+  if (Object.prototype.hasOwnProperty.call(copy, 'title')) {
+    if (typeof copy.title === 'string') {
+      output.title = copy.title
+    }
+    else if (copy.title?._content) {
+      output.title = copy.title._content
+    }
+  }
+
+  if (Object.prototype.hasOwnProperty.call(copy, 'username')) {
+    output.username = copy.username!
+  }
+
+  if (Object.prototype.hasOwnProperty.call(copy, 'primary')) {
+    output.primary = copy.primary!
+  }
+
+  if (Object.prototype.hasOwnProperty.call(copy, 'count_views')) {
+    if (typeof copy.count_views !== 'undefined') {
+      output.views = Number.parseInt(copy.count_views, 10)
+    }
+  }
+
+  if (Object.prototype.hasOwnProperty.call(copy, 'count_comments')) {
+    if (copy.count_comments) {
+      output.comments = Number.parseInt(copy.count_comments, 10)
+    }
+  }
+
+  if (Object.prototype.hasOwnProperty.call(copy, 'count_photos')) {
+    if (typeof copy.count_photos !== 'undefined') {
+      output.photos = copy.count_photos
+    }
+  }
+
+  if (Object.prototype.hasOwnProperty.call(copy, 'count_videos')) {
+    if (typeof copy.count_videos !== 'undefined') {
+      output.videos = copy.count_videos
+    }
+  }
+
+  if (Object.prototype.hasOwnProperty.call(copy, 'date_create')) {
+    if (copy.date_create) {
+      output.date_create = new Date(+copy.date_create * 1000)
+    }
+  }
+
+  if (Object.prototype.hasOwnProperty.call(copy, 'date_update')) {
+    if (copy.date_update) {
+      output.date_last_update = new Date(+copy.date_update * 1000)
+    }
+  }
+
+  const hasImageUrls = Object.keys(copy).some(key => key.startsWith('url_'))
+
+  if (!hasImageUrls) {
+    // Early return so we don't have to create the imageUrls object
+    return output
   }
 
   output.imageUrls = {}
