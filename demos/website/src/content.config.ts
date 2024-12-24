bccd3e4590e50e85fae765c64db089b925b88a91
LekoArts <lekoarts@gmail.com>
2024-12-24 16:40:08 +0100

    try process.env

--- demos/website/src/content.config.ts
+++ demos/website/src/content.config.ts
@@ -1,16 +1,21 @@
+/* eslint-disable node/prefer-global/process */
 import { flickrPeopleGetPhotosLoader, flickrPhotosetsGetListLoader, flickrPhotosetsGetListWithPhotosLoader, flickrPhotosetsGetPhotosLoader } from '@lekoarts/flickr-loader'
 import { defineCollection } from 'astro:content'
 import { FLICKR_USERNAME } from './constants'
 
+const api_key = import.meta.env.FLICKR_API_KEY || process.env.FLICKR_API_KEY
+
 const peopleGetPhotos = defineCollection({
   loader: flickrPeopleGetPhotosLoader({
     username: FLICKR_USERNAME,
+    api_key,
   }),
 })
 
 const photosetsGetList = defineCollection({
   loader: flickrPhotosetsGetListLoader({
     username: FLICKR_USERNAME,
+    api_key,
   }),
 })
 
@@ -18,6 +23,7 @@ const photosetsGetPhotos = defineCollection({
   loader: flickrPhotosetsGetPhotosLoader({
     username: FLICKR_USERNAME,
     photoset_id: '72177720313250218',
+    api_key,
   }),
 })
 
@@ -25,6 +31,7 @@ const photosetsGetListWithPhotos = defineCollection({
   loader: flickrPhotosetsGetListWithPhotosLoader({
     username: FLICKR_USERNAME,
     in: ['72177720317993398', '72177720317980095'],
+    api_key,
   }),
 })
 
