434d56901e192f012f6a1aeaf66dd7687eea8e95
LekoArts <lekoarts@gmail.com>
2025-07-09 12:09:32 +0200

    initial

---
+++ packages/trakt/src/ky.ts
@@ -0,0 +1,12 @@
+import type { KyInstance } from 'ky'
+import ky from 'ky'
+
+export function createTrakt(api_key: string, api_url: string): KyInstance {
+  return ky.create({
+    headers: {
+      'user-agent': '@lekoarts/trakt-loader',
+      'authorization': `Bearer ${api_key}`,
+    },
+    prefixUrl: api_url,
+  })
+}
