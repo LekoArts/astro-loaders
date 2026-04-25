d21800a8f73051be6724b1f8f6b0427ba9274171
LekoArts <lekoarts@gmail.com>
2026-04-25 11:33:08 +0200

    update clerk

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,10 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   typescript: true,
+  ignores: [
+    'packages/clerk/openapi/*.yml',
+    'packages/clerk/src/openapi.ts',
+  ],
   rules: {
     'pnpm/json-enforce-catalog': 'off',
     'pnpm/yaml-enforce-settings': 'off',
